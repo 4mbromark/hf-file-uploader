41993a986841d15dcdd9b10f13dc71c4060437b1
4mbromark <marco.ambrosi2000@gmail.com>
2021-09-25 19:49:41 +0200

    ws-pc-change

--- projects/hf-file-uploader/src/lib/hf-file-uploader.component.ts
+++ projects/hf-file-uploader/src/lib/hf-file-uploader.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'lib-hf-file-uploader',
-  template: `
-    <p>
-      hf-file-uploader works!
-    </p>
-  `,
-  styles: []
+  selector: 'hf-file-uploader',
+  templateUrl: './hf-file-uploader.component.html',
+  styleUrls: ['./hf-file-uploader.component.css']
 })
 export class HfFileUploaderComponent implements OnInit {
+  @Input() text: string;
+  @Input() format: string;
+  @Output() file = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleFileInput(files: FileList): void {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      this.file.emit(fileReader.result.toString());
+    };
+    fileReader.readAsText(files.item(0));
+  }
 }
